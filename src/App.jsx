279fbf5c00007e0bb8f062f51b873ebd2c1fa5b4
tsots <mosalatsotetsi@gmail.com>
2025-03-08 02:05:35 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Tired of listening to sillly people!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut officiis
+        ipsam minus dignissimos facilis dolorum pariatur, optio quos, vitae
+        magnam necessitatibus, voluptatum rerum? Officiis placeat ab hic quo
+        libero quis?
+      </p>
+    </>
+  );
+}
+
+export default App;
